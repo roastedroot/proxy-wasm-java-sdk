8ad9d0838051b6c7937118d74129d6ebd3e04b2b
Hiram Chirino <hiram@hiramchirino.com>
2025-03-14 15:15:50 -0400

    docs: provide more exact information about where the tests originated from.
    Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>

--- src/test/go-examples/dispatch_call_on_tick/README.md
+++ src/test/go-examples/dispatch_call_on_tick/README.md
@@ -1,5 +1,5 @@
 ## Attribution
 
 This example originally came from:
-https://github.com/proxy-wasm/proxy-wasm-go-sdk/tree/main/examples/dispatch_call_on_tick
+https://github.com/proxy-wasm/proxy-wasm-go-sdk/blob/ab4161dcf9246a828008b539a82a1556cf0f2e24/examples/dispatch_call_on_tick
 
--- src/test/java/io/roastedroot/proxywasm/HttpHeadersTest.java
+++ src/test/java/io/roastedroot/proxywasm/HttpHeadersTest.java
@@ -11,6 +11,9 @@ import java.nio.file.Path;
 import java.util.Map;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Java port of https://github.com/proxy-wasm/proxy-wasm-go-sdk/blob/ab4161dcf9246a828008b539a82a1556cf0f2e24/examples/http_headers/main_test.go
+ */
 public class HttpHeadersTest {
 
     private MockHandler handler = new MockHandler();
