d26e4ead3f23284b22b5ee4cd6344674464ad723
Hiram Chirino <hiram@hiramchirino.com>
2025-03-17 22:40:24 -0400

    Implementing more of the JAXRS filter.
    Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>

--- proxy-wasm-jaxrs/src/main/java/io/roastedroot/proxywasm/jaxrs/ProxyWasmFilter.java
+++ proxy-wasm-jaxrs/src/main/java/io/roastedroot/proxywasm/jaxrs/ProxyWasmFilter.java
@@ -4,11 +4,14 @@ import io.roastedroot.proxywasm.StartException;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.container.ContainerRequestContext;
 import jakarta.ws.rs.container.ContainerRequestFilter;
+import jakarta.ws.rs.container.ContainerResponseContext;
+import jakarta.ws.rs.container.ContainerResponseFilter;
 import jakarta.ws.rs.container.PreMatching;
+import jakarta.ws.rs.core.Response;
 import java.io.IOException;
 
 @PreMatching
-public class ProxyWasmFilter implements ContainerRequestFilter {
+public class ProxyWasmFilter implements ContainerRequestFilter, ContainerResponseFilter {
 
     private final WasmPluginFactory pluginFactory;
 
@@ -18,11 +21,27 @@ public class ProxyWasmFilter implements ContainerRequestFilter {
     }
 
     @Override
-    public void filter(ContainerRequestContext containerRequestContext) throws IOException {
+    public void filter(ContainerRequestContext requestContext) throws IOException {
+
+        WasmPlugin plugin = null;
         try {
-            var plugin = pluginFactory.create();
-            System.out.println("Filtering request with plugin: " + plugin.name());
-        } catch (StartException ignored) {
+            plugin = pluginFactory.create();
+        } catch (StartException e) {
+            requestContext.abortWith(
+                    Response.status(Response.Status.INTERNAL_SERVER_ERROR).build());
         }
+
+        // so we can continue to use the plugin in the other filter methods.
+        requestContext.setProperty("WasmPlugin", plugin);
+        var wasmHttpContext = plugin.createHttpContext();
+        requestContext.setProperty("WasmHttpContext", wasmHttpContext);
+
+        plugin.handler().setHttpRequestHeaders(requestContext.getHeaders());
+        wasmHttpContext.callOnRequestHeaders(false);
     }
+
+    @Override
+    public void filter(
+            ContainerRequestContext requestContext, ContainerResponseContext responseContext)
+            throws IOException {}
 }
--- proxy-wasm-jaxrs/src/main/java/io/roastedroot/proxywasm/jaxrs/WasmPlugin.java
+++ proxy-wasm-jaxrs/src/main/java/io/roastedroot/proxywasm/jaxrs/WasmPlugin.java
@@ -3,8 +3,10 @@ package io.roastedroot.proxywasm.jaxrs;
 import com.dylibso.chicory.runtime.ImportMemory;
 import com.dylibso.chicory.runtime.Instance;
 import com.dylibso.chicory.wasm.WasmModule;
+import io.roastedroot.proxywasm.HttpContext;
 import io.roastedroot.proxywasm.ProxyWasm;
 import io.roastedroot.proxywasm.StartException;
+import java.util.Objects;
 
 public class WasmPlugin {
 
@@ -13,6 +15,9 @@ public class WasmPlugin {
     private final JaxrsHandler handler;
 
     public WasmPlugin(String name, ProxyWasm proxyWasm, JaxrsHandler handler) {
+        Objects.requireNonNull(name);
+        Objects.requireNonNull(proxyWasm);
+        Objects.requireNonNull(handler);
         this.name = name;
         this.proxyWasm = proxyWasm;
         this.handler = handler;
@@ -20,6 +25,18 @@ public class WasmPlugin {
 
     public String name() {
         return name;
+    }
+
+    ProxyWasm proxyWasm() {
+        return proxyWasm;
+    }
+
+    HttpContext createHttpContext() {
+        return proxyWasm.createHttpContext(this.handler);
+    }
+
+    JaxrsHandler handler() {
+        return handler;
     }
 
     public static WasmPlugin.Builder builder() {
