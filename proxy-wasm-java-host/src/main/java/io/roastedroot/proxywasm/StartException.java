9f4ab01d755398890ac97f617dfcdeeadab72cb8
Hiram Chirino <hiram@hiramchirino.com>
2025-04-21 13:48:40 -0400

    Simplify the plugin builder api.  Add some javadoc.
    Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>

--- proxy-wasm-java-host/src/main/java/io/roastedroot/proxywasm/StartException.java
+++ proxy-wasm-java-host/src/main/java/io/roastedroot/proxywasm/StartException.java
@@ -4,4 +4,8 @@ public class StartException extends Exception {
     public StartException(String message) {
         super(message);
     }
+
+    public StartException(String message, Throwable cause) {
+        super(message, cause);
+    }
 }
