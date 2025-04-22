f74d9681d80155cbca6c15f3fa812b489ab8c91c
Hiram Chirino <hiram@hiramchirino.com>
2025-04-22 09:36:16 -0400

    Add javadoc to the jaxrs module.
    Signed-off-by: Hiram Chirino <hiram@hiramchirino.com>

--- proxy-wasm-jaxrs/src/main/java/io/roastedroot/proxywasm/jaxrs/WasmPluginFeature.java
+++ proxy-wasm-jaxrs/src/main/java/io/roastedroot/proxywasm/jaxrs/WasmPluginFeature.java
@@ -5,14 +5,78 @@ import io.roastedroot.proxywasm.StartException;
 import io.roastedroot.proxywasm.internal.ServerAdaptor;
 import io.roastedroot.proxywasm.jaxrs.internal.AbstractWasmPluginFeature;
 import java.util.Arrays;
+import java.util.List;
 
 /**
- * WasmPluginFeature is a JAX-RS feature that allows the use of Proxy-Wasm plugins to filter JAX-RS
- * requests.
+ * A JAX-RS {@link jakarta.ws.rs.core.Feature} that enables Proxy-Wasm plugin filtering
+ * for JAX-RS applications. This feature registers the necessary {@link WasmPluginFilter}
+ * to intercept requests and responses for resources annotated with {@link WasmPlugin}.
+ *
+ * <p>To use this feature, register an instance of it with your JAX-RS application, providing
+ * the required {@link ServerAdaptor} and a list of {@link PluginFactory} instances.
+ *
+ * <p>If you are using a CDI container like quarkus, you will be using the
+ * {@link io.roastedroot.proxywasm.jaxrs.cdi.WasmPluginFeature} instead.
+ *
+ * <pre>{@code
+ * public class MyApplication extends jakarta.ws.rs.core.Application {
+ *     @Override
+ *     public Set<Class<?>> getClasses() {
+ *         Set<Class<?>> resources = new HashSet<>();
+ *         resources.add(MyResource.class);
+ *         return resources;
+ *     }
+ *
+ *     @Override
+ *     public Set<Object> getSingletons() {
+ *         Set<Object> singletons = new HashSet<>();
+ *         try {
+ *             // Assuming a ServerAdaptor and PluginFactory are available
+ *             ServerAdaptor serverAdaptor = ...;
+ *             PluginFactory myPluginFactory = ...;
+ *             singletons.add(new WasmPluginFeature(serverAdaptor, myPluginFactory));
+ *         } catch (StartException e) {
+ *             throw new RuntimeException("Failed to initialize WasmPluginFeature", e);
+ *         }
+ *         return singletons;
+ *     }
+ * }
+ * }</pre>
+ *
+ * @see WasmPlugin
+ * @see WasmPluginFilter
+ * @see PluginFactory
+ * @see ServerAdaptor
  */
 public class WasmPluginFeature extends AbstractWasmPluginFeature {
+
+    /**
+     * Constructs a new WasmPluginFeature.
+     *
+     * @param httpServer The {@link ServerAdaptor} used to adapt JAX-RS specific request/response
+     *                   objects for the Proxy-Wasm host.
+     * @param factories  One or more {@link PluginFactory} instances used to create and manage
+     *                   the Proxy-Wasm plugins.
+     * @throws StartException If an error occurs during the initialization or startup of the
+     *                        underlying Proxy-Wasm plugins.
+     */
     public WasmPluginFeature(ServerAdaptor httpServer, PluginFactory... factories)
             throws StartException {
-        init(Arrays.asList(factories), httpServer);
+        this(httpServer, Arrays.asList(factories));
+    }
+
+    /**
+     * Constructs a new WasmPluginFeature with a list of factories.
+     *
+     * @param httpServer The {@link ServerAdaptor} used to adapt JAX-RS specific request/response
+     *                   objects for the Proxy-Wasm host.
+     * @param factories  A list of {@link PluginFactory} instances used to create and manage
+     *                   the Proxy-Wasm plugins.
+     * @throws StartException If an error occurs during the initialization or startup of the
+     *                        underlying Proxy-Wasm plugins.
+     */
+    public WasmPluginFeature(ServerAdaptor httpServer, List<PluginFactory> factories)
+            throws StartException {
+        init(factories, httpServer);
     }
 }
--- proxy-wasm-jaxrs/src/main/java/io/roastedroot/proxywasm/jaxrs/cdi/ServerAdaptor.java
+++ proxy-wasm-jaxrs/src/main/java/io/roastedroot/proxywasm/jaxrs/cdi/ServerAdaptor.java
@@ -1,13 +1,36 @@
 package io.roastedroot.proxywasm.jaxrs.cdi;
 
+import io.roastedroot.proxywasm.jaxrs.internal.BlockingServerAdaptor;
 import jakarta.annotation.Priority;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.Alternative;
 
 /**
- * ServerAdaptor is a CDI alternative for the {@link io.roastedroot.proxywasm.jaxrs.internal.ServerAdaptor}
+ * A CDI {@link Alternative} bean providing an implementation of
+ * {@link io.roastedroot.proxywasm.internal.ServerAdaptor} tailored for JAX-RS environments
+ * managed by CDI (Contexts and Dependency Injection).
+ *
+ * <p>This bean allows the Proxy-Wasm host integration to be seamlessly managed within
+ * a CDI container (like Quarkus or Helidon). By being an {@code @Alternative} with a
+ * specific {@code @Priority}, it can be selected or overridden as needed within the
+ * CDI application configuration.
+ *
+ * <p>It extends {@link BlockingServerAdaptor}, inheriting the
+ * base JAX-RS adaptation logic.
+ *
+ * @see io.roastedroot.proxywasm.internal.ServerAdaptor
+ * @see Alternative
+ * @see ApplicationScoped
  */
 @Alternative
 @Priority(100)
 @ApplicationScoped
-public class ServerAdaptor extends io.roastedroot.proxywasm.jaxrs.internal.ServerAdaptor {}
+public class ServerAdaptor extends BlockingServerAdaptor {
+
+    /**
+     * Default constructor required by CDI for proxying and bean management.
+     */
+    public ServerAdaptor() {
+        super();
+    }
+}
